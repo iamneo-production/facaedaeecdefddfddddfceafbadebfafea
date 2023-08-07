cc4535e3b9327508b9eacd924f94d0770be91d20
Iamneo <techadmin@iamneo.ai>
2023-08-07 12:12:02 +0000

    Backup Commit

--- springapp/src/test/java/com/examly/springapp/SpringApplicationTests.java
+++ springapp/src/test/java/com/examly/springapp/SpringApplicationTests.java
@@ -1,4 +1,4 @@
-package qatechhub.assignments;
+package com.examly.springapp;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -18,41 +18,29 @@ public class EbaySearch {
 	public void invokeBrowser(){
 		
 		
-		
-		
+		ChromeOptions chromeOptions = new ChromeOptions();
+        WebDriver driver = new RemoteWebDriver(new URL("http://34.85.242.216:4444"), chromeOptions);
 		driver.manage().window().maximize();
-		
 		driver.manage().deleteAllCookies();
-		
 		driver.manage().timeouts().pageLoadTimeout(90, TimeUnit.SECONDS);
-		
 		driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
-		
 		driver.get(url);
-		
 	}
 	
 	public void searchProduct(String product, String category){
 		
 		driver.findElement(By.id("gh-ac")).sendKeys(product);
-		
 		WebElement dropdown = driver.findElement(By.id("gh-cat"));
 		Select selectCategory = new Select(dropdown);
-		
 		selectCategory.selectByVisibleText(category);
-		
 		driver.findElement(By.id("gh-btn")).click();
-		
 		String result = driver.findElement(By.className("listingscnt")).getText();
-		
 		System.out.println("Result :: "+ result);
 	}
 	
 	public void getNthProduct(int itemNumber){
 		String productXpath = String.format("//div[@id='ResultSetItems']/ul[@id='ListViewInner']/li[%d]", itemNumber);
-		
 		String nthProduct = driver.findElement(By.xpath(productXpath)).getText();
-		
 		System.out.println("Nth Product :: "+ nthProduct);
 	}
 	
@@ -61,7 +49,6 @@ public class EbaySearch {
 	
 	for(WebElement product: allProduct){
 		System.out.println(product.getText());
-		
 		System.out.println("-----------------------------------------");
 	}
 	}
@@ -69,16 +56,13 @@ public class EbaySearch {
 	public void getAllProductsViaScrollDown(){
 		List<WebElement> allProduct = driver.findElements(By.xpath("//div[@id='ResultSetItems']/ul[@id='ListViewInner']/li"));
 		Actions action = new Actions(driver);
-		
 		for(WebElement product: allProduct){
 			
 			action.moveToElement(product).build().perform();
-			
 			System.out.println(product.getText());
-			
 			System.out.println("-----------------------------------------");
 		}
-		}
+	}
 	
 	public void getAllProductsViaScrollDownViaJS(){
 		List<WebElement> allProduct = driver.findElements(By.xpath("//div[@id='ResultSetItems']/ul[@id='ListViewInner']/li"));
@@ -87,27 +71,18 @@ public class EbaySearch {
 		for(WebElement product: allProduct){
 			
 			int x, y;
-			
 			x = product.getLocation().x;
-			
 			y = product.getLocation().y;
-			
 			scrollDown(x, y);
-			
 			System.out.println(product.getText());
-			
 			System.out.println("-----------------------------------------");
 		}
 		}
 	
 		private void scrollDown(int x, int y){
-			
 			JavascriptExecutor jsEngine;
-			
 			String jsCommand = String.format("window.scrollBy(%d,%d)", x,y);
-			
 			jsEngine = (JavascriptExecutor) driver;
-			
 			jsEngine.executeScript(jsCommand);
-		}
+	}
 }
